f210a5afb36990fc6f151a31aee4f62e3c27af03
freedomaws2018 <44969695+freedomaws2018@users.noreply.github.com>
2019-05-24 16:08:39 +0800

    2019-05-24-16-08

--- src/main/java/com/example/demo/DataBase/Repository/MenuRepository.java
+++ src/main/java/com/example/demo/DataBase/Repository/MenuRepository.java
@@ -10,5 +10,12 @@ import com.example.demo.DataBase.Entity.Menu;
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, String> {
 
-  List<Menu> findByLevel(Integer levle);
+  List<Menu> findByLevel(Integer level);
+
+//  @Query(value = "SELECT * FROM tbl_menu WHERE level > 1 AND parent_menu = ?1 ;", nativeQuery = true)
+  /** SELECT * FROM tbl_menu WHERE level = ? AND parent_menu_name = ? **/
+//  List<Menu> findByLevelEqualsAndParentMenuName(Integer level , String parentMenuName);
+//
+//  List<Menu> findByParentMenuName(String parentMenuName);
+
 }
